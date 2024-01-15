b63323fc013da24b6a4c579bf246304bc938179b
cmne3602 <53000757+cmne3602@users.noreply.github.com>
2024-01-15 15:05:01 +0900

    01.13

--- src/App.js
+++ src/App.js
@@ -2,76 +2,53 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 
-function Article(props) {
-  return (
-    <article>
-    <h2>{props.title}</h2>
-    {props.body}
-  </article>
-  )
-}
-
-function Header(props) {
-  return <header>
-  <h1><a href="/" onClick={(event)=>{
-    event.preventDefault();
-    props.onChangeMode();
-  }}>{props.title}</a></h1>
-  </header>
-}
-
-function Nav(props) {
-  const lis = []
-  for(let i=0; i<props.topics.length; i++){
-    let t = props.topics[i];
-    lis.push(<li key={i + 1}>
-      <a id={t.id} href={'/read/' + t.id} onClick={(event)=>{
-        event.preventDefault();
-        props.onChangeMode(Number(event.target.id));
-      }}>{t.title}</a>
-      </li>)
-  }
-  return <nav>
-    <ol>
-      {lis}
-    </ol>
-  </nav>
-}
-
 function App() {
-  const [mode, setMode] = useState('WELCOME');
-  const [id, setId] = useState(null);
-  const topics = [
-    {id:1, title:'html', body:'html is ...'},
-    {id:2, title:'css', body:'css is ...'},
-    {id:3, title:'javascript', body:'js is ...'},
-  ]
-  let content = null;
-  if(mode === 'WELCOME'){
-    content = <Article title="Welcome" body="Hello, Web"></Article>
-  } else if(mode === 'READ'){
-    let title, body = null;
-    for(let i = 0; i < topics.length; i++){
-      if(topics[i].id === id){
-        title = topics[i].title;
-        body = topics[i].body;
-      }
-    }
-
-    content = <Article title={title} body={body}></Article>
-  }
-
+  let post = '강남 우동 맛집';
+  let [logo, setLogo] = useState(['React Blog', 'Hamberger Blog', 'Shoes Blog']);
+  let [like, setLike] = useState(0);
   return (
-    <div>
-      <Header title="React" onChangeMode={()=>{
-        setMode('WELCOME');
-      }}></Header>
-      <Nav topics={topics} onChangeMode={(_id)=>{
-        setMode('READ');
-        setId(_id);
-      }}></Nav>
-      {content}
+    <div className='App'>
+      <div className='black-nav'>
+        <h4>블로그임</h4>
+      </div>
+
+      <button onClick={()=>{
+        let temp = [...logo];
+        temp.sort();
+        setLogo(temp);
+      }}>가나다순 정렬</button>
+
+      <div className='list'>
+        <h4>{ logo[0] }<span onClick={ ()=>{
+          setLike(like + 1);
+          let temp = [...logo];
+          temp[0] = 'React Hi';
+          setLogo(temp);
+        } }>👍</span>{ like }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ logo[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ logo[2] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      <Modal></Modal>
+
     </div>
+  );
+}
+
+function Modal() {
+  return(
+    <div className='modal'>
+    <h4>제목</h4>
+    <p>날짜</p>
+    <p>상세내용</p>
+  </div>
   );
 }
 
